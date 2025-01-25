956d6ac0bde12bb5d79887ca88b525b37c078227
krlosbyte <carloz91@live.com.mx>
2025-01-25 01:26:58 -0600

    Funcionando

--- app.js
+++ app.js
@@ -7,10 +7,32 @@ function agregarAmigo() {
         alert("Por favor, inserte un nombre.")
     } else{
         amigos.push(obtenerNombre);
-        return limpiarForm();
+        limpiarForm();
+        mostrarNombre();
     }
 }
 
 function limpiarForm() {
-    document.getElementById('amigo').value = '';
+    document.getElementById('amigo').value = ''; 
 }
+
+//En esta funcion use IA para orientarme
+function mostrarNombre() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+      let nuevoAmigo = document.createElement("li");
+      nuevoAmigo.textContent = amigos[i];
+      lista.appendChild(nuevoAmigo);
+    }
+  }
+
+  function seleccionarAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos agregados");
+    } else{
+        let elegirAmigo = Math.floor(Math.random() * amigos.length);
+        let random = document.getElementById("resultado");
+        random.innerHTML = `<li>${amigos[elegirAmigo]}</li>`;
+    }
+  }
