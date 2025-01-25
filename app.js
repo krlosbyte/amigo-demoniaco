d271514623d936e8c8f7dccbc76b1f4a7568bbfc
krlosbyte <carloz91@live.com.mx>
2025-01-24 23:16:39 -0600

    Función para añadir amigo

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+console.log(amigos);
+
+function agregarAmigo() {
+    let obtenerNombre = document.getElementById('amigo').value;
+    if (obtenerNombre == '') {
+        alert("Por favor, inserte un nombre.")
+    } else{
+        amigos.push(obtenerNombre);
+        return limpiarForm();
+    }
+}
+
+function limpiarForm() {
+    document.getElementById('amigo').value = '';
+}
